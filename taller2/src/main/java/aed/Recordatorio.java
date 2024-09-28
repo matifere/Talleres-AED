d1c075c7049cc3588fdb00bb2e33a59b2dc6569c
matifere <34757838+matifere@users.noreply.github.com>
2024-09-28 19:42:30 -0300

    Update Recordatorio.java
    hasta la parte 2 del taller

--- taller2/src/main/java/aed/Recordatorio.java
+++ taller2/src/main/java/aed/Recordatorio.java
@@ -26,13 +26,19 @@ public class Recordatorio {
 
     @Override
     public String toString() {
-        return "mensaje @ " + mensajeClase + " " + fechaClase + " " + horarioClase;
+        return mensajeClase + " @ " + fechaClase + " " + horarioClase;
     }
 
     @Override
-    public boolean equals(Object otro) {
-        // Implementar
-        return true;
+    public boolean equals(Object otro) { //falta el test de esto (supongo que esta bien igual es identico al resto)
+        boolean otroEsNull = (otro == null);
+        boolean claseDist = otro.getClass() != this.getClass();
+        if (otroEsNull || claseDist) {
+            return false;
+        }
+        Recordatorio otroRecordatorio = (Recordatorio) otro;
+        return mensajeClase == otroRecordatorio.mensajeClase && horarioClase == otroRecordatorio.horarioClase
+                && otroRecordatorio.fechaClase == fechaClase;
     }
 
 }
