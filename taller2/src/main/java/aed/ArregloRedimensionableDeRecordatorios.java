538f43130530a03973d7a30ec1c3fa8a18253576
matifere <34757838+matifere@users.noreply.github.com>
2024-09-29 00:13:24 -0300

    Update ArregloRedimensionableDeRecordatorios.java
    hasta el 15

--- taller2/src/main/java/aed/ArregloRedimensionableDeRecordatorios.java
+++ taller2/src/main/java/aed/ArregloRedimensionableDeRecordatorios.java
@@ -2,65 +2,70 @@ package aed;
 
 class ArregloRedimensionableDeRecordatorios {
 
-    private Recordatorio[] listaRecordatorios = new Recordatorio[0];
+    private Recordatorio[] arregloRecordatorios = new Recordatorio[0];
 
     public ArregloRedimensionableDeRecordatorios() {
 
     }
 
     public int longitud() {
-        return listaRecordatorios.length;
+        return arregloRecordatorios.length;
     }
 
     public void agregarAtras(Recordatorio i) { // preguntar si esta bien la implementacion
-        Recordatorio[] arraySobrescribir = new Recordatorio[listaRecordatorios.length + 1];
-        for (int j = 0; j < listaRecordatorios.length; j++) {
-            arraySobrescribir[j] = listaRecordatorios[j];
+        Recordatorio[] arraySobrescribir = new Recordatorio[arregloRecordatorios.length + 1];
+        for (int j = 0; j < arregloRecordatorios.length; j++) {
+            arraySobrescribir[j] = arregloRecordatorios[j];
         }
         // agrego el ultimo elemento al arreglo
         arraySobrescribir[arraySobrescribir.length - 1] = i;
-        listaRecordatorios = arraySobrescribir;
+        arregloRecordatorios = arraySobrescribir;
     }
 
     public Recordatorio obtener(int i) {
-        return listaRecordatorios[i];
+        return arregloRecordatorios[i];
     }
 
     public void quitarAtras() {
-        Recordatorio[] arraySobrescribir = new Recordatorio[listaRecordatorios.length - 1];
-        for (int j = 0; j < listaRecordatorios.length - 1; j++) {
-            arraySobrescribir[j] = listaRecordatorios[j];
+        Recordatorio[] arraySobrescribir = new Recordatorio[arregloRecordatorios.length - 1];
+        for (int j = 0; j < arregloRecordatorios.length - 1; j++) {
+            arraySobrescribir[j] = arregloRecordatorios[j];
         }
         // saco el ultimo elemento al arreglo
-        listaRecordatorios = arraySobrescribir;
+        arregloRecordatorios = arraySobrescribir;
     }
 
     public void modificarPosicion(int indice, Recordatorio valor) {
-        Recordatorio[] arraySobrescribir = new Recordatorio[listaRecordatorios.length];
-        if (indice >= listaRecordatorios.length) { // por las dudas puse esto pero no se si es necesario
+        Recordatorio[] arraySobrescribir = new Recordatorio[arregloRecordatorios.length];
+        if (indice >= arregloRecordatorios.length) { // por las dudas puse esto pero no se si es necesario
             return;
         }
         // primero copio el arreglo hasta la posicion indice
         for (int i = 0; i < indice; i++) {
-            arraySobrescribir[i] = listaRecordatorios[i];
+            arraySobrescribir[i] = arregloRecordatorios[i];
         }
         // cambio el valor en el indice
         arraySobrescribir[indice] = valor;
         // sigo copiando (si es que hay algo mas para copiar)
-        if (indice < listaRecordatorios.length) {
-            for (int i = indice + 1; i < listaRecordatorios.length; i++) {
-                arraySobrescribir[i] = listaRecordatorios[i];
+        if (indice < arregloRecordatorios.length) {
+            for (int i = indice + 1; i < arregloRecordatorios.length; i++) {
+                arraySobrescribir[i] = arregloRecordatorios[i];
             }
         }
-        listaRecordatorios = arraySobrescribir;
+        arregloRecordatorios = arraySobrescribir;
     }
 
     public ArregloRedimensionableDeRecordatorios(ArregloRedimensionableDeRecordatorios vector) {
-        // Implementar
+        this.arregloRecordatorios = new Recordatorio[vector.arregloRecordatorios.length]; 
+        for(int i = 0; i < vector.arregloRecordatorios.length; i++){
+            this.arregloRecordatorios[i] = vector.arregloRecordatorios[i]; 
+        }
+        //la cantidad de operaciones new es del largo del arreglo (pues cada uno de sus elementos contiene uno) mas 1 que seria el de la primera linea del constructor
     }
 
     public ArregloRedimensionableDeRecordatorios copiar() {
-        // Implementar
-        return null;
+        ArregloRedimensionableDeRecordatorios copia = new ArregloRedimensionableDeRecordatorios(this);
+        return copia;
+        //aca se le suma un new a la cuenta anterior
     }
 }
